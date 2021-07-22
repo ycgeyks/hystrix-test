1ab111dee9a53799de4d6f917cd739fe4977304e
Ryan <84768331@qq.com>
2021-07-22 11:57:32 +0800

    add test method,not success

---
+++ first-hy-client/src/main/java/org/crazyit/cloud/error/CommandHelloFailure.java
@@ -0,0 +1,35 @@
+package org.crazyit.cloud.error;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommandHelloFailure extends HystrixCommand<String> {
+
+    private final String name;
+
+    public CommandHelloFailure(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
+        this.name = name;
+    }
+
+    @Override
+    protected String run() {
+        throw new RuntimeException("this command always fails");
+    }
+
+    @Override
+    protected String getFallback() {
+        return "Hello Failure " + name + "!";
+    }
+
+    @Test
+    public void testSynchronous() {
+        assertEquals("Hello Failure World!", new CommandHelloFailure("World").execute());
+        assertEquals("Hello Failure Bob!", new CommandHelloFailure("Bob").execute());
+    }
+
+}
+
